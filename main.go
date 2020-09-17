d6752aee3e092b7d6fff461b2a5b3b48b736faaa
jdxj <985759262@qq.com>
2020-09-17 14:25:19 +0800

    refactor: 注册 handler

--- main.go
+++ main.go
@@ -2,35 +2,32 @@ package main
 
 import (
 	"github.com/jdxj/logger"
-	"github.com/jdxj/user/config"
 	"github.com/jdxj/user/handler"
-	"github.com/jdxj/user/model"
-
+	user "github.com/jdxj/user/proto"
 	"github.com/micro/micro/v3/service"
 )
 
 func main() {
-	err := config.Init("config.yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	logger.NewPathMode(config.Log().Path, config.Mode())
-
-	dbCfg := config.DB()
-	err = model.InitDB(dbCfg.User, dbCfg.Password, dbCfg.Host, dbCfg.DBName)
-	if err != nil {
-		panic(err)
-	}
+	//err := config.Init("/home/jdxj/workspace/user/config.yaml")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//logger.NewPathMode(config.Log().Path, config.Mode())
+	//
+	//dbCfg := config.DB()
+	//err = model.InitDB(dbCfg.User, dbCfg.Password, dbCfg.Host, dbCfg.DBName)
+	//if err != nil {
+	//	panic(err)
+	//}
 	// Create service
 	srv := service.New(
 		service.Name("user"),
-		//service.Version("latest"),
+		service.Version("latest"),
 	)
 
 	// Register handler
-	//pb.RegisterUserHandler(srv.Server(), new(handler.User))
-	srv.Handle(new(handler.User))
+	user.RegisterUserHandler(srv.Server(), new(handler.User))
 
 	// Run service
 	if err := srv.Run(); err != nil {
